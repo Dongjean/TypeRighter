cb5c0f44e9a1ccfbd287d5ff28b29b7f7ac801b8
Dongjean <shindongjin2004@gmail.com>
2026-05-19 21:09:52 +0800

    test: use parametrization to allow all other test cases to run after some fail

--- test_main.py
+++ test_main.py
@@ -1,9 +1,12 @@
 def add(x, y):
     return x + y
 
-def test_add():
-    assert add(1, 2) == 3
-    assert add(6, 7) == 13
-    assert add(1, -1) == 0
-    assert add(5, 5) == 9 # Wrong example
-    assert add(-5, -9) == -14
+@pytest.mark.parametrize("x, y, expected", [
+    (1, 2, 3),
+    (6, 7, 13),
+    (1, -1, 0),
+    (5, 5, 9), # wrong example
+    (-5, -9, -14) # this runs too
+])
+def test_add(x, y, expected):
+    assert add(x, y) == expected
