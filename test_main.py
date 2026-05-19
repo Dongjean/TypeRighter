66ee918763e776f0401047cdb8a177ee25d33420
Dongjean <shindongjin2004@gmail.com>
2026-05-19 21:06:31 +0800

    fix: add function name to test calls

--- test_main.py
+++ test_main.py
@@ -4,6 +4,6 @@ def add(x, y):
 def test_add():
     assert add(1, 2) == 3
     assert add(6, 7) == 13
-    assert (1, -1) == 0
-    assert (5, 5) == 9 # Wrong example
-    assert (-5, -9) == -14
+    assert add(1, -1) == 0
+    assert add(5, 5) == 9 # Wrong example
+    assert add(-5, -9) == -14
