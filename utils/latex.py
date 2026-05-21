3c0c280931edda6a31d3f402aee3fabcef9e1353
Dongjean <shindongjin2004@gmail.com>
2026-05-21 22:45:20 +0800

    refactor: improve comments

--- utils/latex.py
+++ utils/latex.py
@@ -21,13 +21,16 @@ def display_latex_window(output_frame, equation, background_color):
     # equation = r'$\frac{-b \pm \sqrt{b^2 - 4ac}}{2a}$'
     ax.text(0.5, 0.5, equation, size=24, ha='center', va='center')
 
-    # Embed the figure into the Tkinter window
+    # Middleman hybrid wrapper for matplotlib figures and tkinter widgets
+    # This is a matplotlib object
     canvas = FigureCanvasTkAgg(fig, master=output_frame)
-    canvas_widget = canvas.get_tk_widget()
-    canvas_widget.pack(fill="both", expand=True)
-
-    # Draw the canvas
+    # Render the canvas on matplotlib's side
     canvas.draw()
+
+    # Extract the underlying tkinter widget from the FigureCanvasTkAgg() object
+    canvas_widget = canvas.get_tk_widget()
+    # Render the canvas on tkinter's side
+    canvas_widget.pack(fill="both", expand=True)
   except Exception as e:
     plt.close('all')
     print(f'error displaying latex: {e}')
