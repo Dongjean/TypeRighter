6b896dcaf32c3ef00fa66c46c1e188c2b8d19a7c
Dongjean <shindongjin2004@gmail.com>
2026-05-25 21:21:03 +0800

    test: fix main.pyw import for pytest

--- pytest/test_bg_listener.py
+++ pytest/test_bg_listener.py
@@ -6,7 +6,13 @@ import importlib.util
 import sys
 from pathlib import Path
 
-main_file_path = Path(__file__).parent / "main.pyw"
+main_file_path = Path(__file__).parent / "../main.pyw"
+main_dir = str(main_file_path.parent)
+
+# Inject this directory to the front of Python's search path
+if main_dir not in sys.path:
+    sys.path.insert(0, main_dir)
+
 spec = importlib.util.spec_from_file_location("main", main_file_path)
 main = importlib.util.module_from_spec(spec)
 sys.modules["main"] = main
