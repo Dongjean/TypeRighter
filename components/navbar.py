4d6a23cf55fcf959588a9e556dc46ce1ad9722d2
ackw130 <andrewchong130@gmail.com>
2026-05-26 22:05:01 +0800

    Updates

---
+++ components/navbar.py
@@ -0,0 +1,38 @@
+import tkinter as tk
+from tkinter import font as tkfont
+
+import views.root_view as root_view
+
+import components.latex_workspace as latex_workspace
+import components.user_auth as user_auth
+
+curr_window = ""
+init_functions = {
+    "latex-workspace": latex_workspace.build_latex_workspace,
+    "user-auth": user_auth.build_user_auth,
+}
+
+def change_window(selected_window, root, COLORS, FONTS):
+    global curr_window
+    if selected_window != curr_window:
+        print(selected_window)
+        curr_window = selected_window
+        if selected_window in init_functions:
+            init_functions[selected_window](root=root, COLORS=COLORS, FONTS=FONTS)
+        else:
+            print(f"No function found to initialise {selected_window}")
+
+def build_navbar(root, COLORS, FONTS, WINDOWS, start_window):
+
+    # Frame for navbar
+    navbar_frame = tk.Frame(root, bg=COLORS["bg_main"], takefocus=True, name="navbar_frame")
+    navbar_frame.pack(side="right", fill="both")
+
+    # Display each window as a custom button
+    navbar_frame.selected_window = tk.StringVar(value=start_window)
+    for key, value in WINDOWS.items():
+        btn = tk.Radiobutton(navbar_frame, text=value["name"], variable=navbar_frame.selected_window, value=key, bg=COLORS["border"], fg=COLORS["text_main"], bd=0, relief="flat", width=10, height=3, font=FONTS["font_subtitle"], selectcolor=COLORS["accent_blue"], activebackground=COLORS["accent_blue"], indicatoron=False, command=(lambda: change_window(navbar_frame.selected_window.get(), root, COLORS, FONTS)))
+        btn.pack(side="top")
+
+    # Manually initialise the first window
+    change_window(start_window, root, COLORS, FONTS)
---
+++ components/user_auth.py
@@ -0,0 +1,55 @@
+import tkinter as tk
+from tkinter import font as tkfont
+
+def login(username_editor, password_editor):
+    username = username_editor.get()
+    password = password_editor.get()
+    print(f"loggin in for {username}, {password}")
+
+def build_user_auth(root, COLORS, FONTS):
+
+    # Reset root for a clean overlay init
+    # Remove all child widgets except for the navbar
+    for widget in root.winfo_children():
+        if widget.winfo_name() != "navbar_frame":
+          widget.destroy()
+    
+    # Frame for user login page
+    auth_frame = tk.Frame(root, bg=COLORS["bg_main"], padx=20, pady=20, takefocus=True, name="auth_frame")
+    auth_frame.pack(side="top", fill="both", expand=True)
+
+    # Header Titles
+    # Set to be at the top, centred (expand=False by default)
+    title_label = tk.Label(auth_frame, text="Login", fg=COLORS["text_main"], bg=COLORS["bg_main"], font=FONTS["font_title"])
+    title_label.pack(fill="x", pady=0)
+
+    # Login Hub Container
+    login_hub_container = tk.Frame(auth_frame, bg=COLORS["bg_main"], bd=0)
+    login_hub_container.pack(expand=True)
+
+    # Username Frame
+    username_frame = tk.Frame(login_hub_container, bg=COLORS["bg_input"], bd=1, highlightbackground=COLORS["border"], highlightthickness=1)
+    username_frame.pack(fill="x", expand=True)
+    username_label = tk.Label(username_frame, text="Username: ", bg=COLORS["bg_input"], fg=COLORS["text_main"], bd=0, font=FONTS["font_subtitle"])
+    username_label.pack(side="left")
+    username_editor = tk.Entry(username_frame, bg=COLORS["bg_input"], fg=COLORS["text_main"], insertbackground="white", bd=0, font=FONTS["font_subtitle"])
+    username_editor.pack(side="right")
+    # Bind the key release event inside text editor to our reader function
+    # tk.Text() has no native function to read text in real time, this is the best option
+  
+    # Password Frame
+    password_frame = tk.Frame(login_hub_container, bg=COLORS["bg_input"], bd=1, highlightbackground=COLORS["border"], highlightthickness=1)
+    password_frame.pack(fill="x", expand=True)
+    password_label = tk.Label(password_frame, text="Password: ", bg=COLORS["bg_input"], fg=COLORS["text_main"], bd=0, font=FONTS["font_subtitle"])
+    password_label.pack(side="left")
+    password_editor = tk.Entry(password_frame, bg=COLORS["bg_input"], fg=COLORS["text_main"], insertbackground="white", bd=0, font=FONTS["font_subtitle"])
+    password_editor.pack(side="right")
+    # Bind the key release event inside text editor to our reader function
+    # tk.Text() has no native function to read text in real time, this is the best option
+
+    # Login Button
+    login_button = tk.Button(login_hub_container, text="Login", bg=COLORS["border"], fg=COLORS["text_main"], bd=0, relief="flat", font=FONTS["font_subtitle"], command=(lambda: login(username_editor, password_editor)))
+    login_button.pack(side="bottom")
+
+    # Clicking anywhere outside the text editor frame makes us lose active focus
+    root.bind("<Button-1>", lambda event: event.widget.focus_set())
