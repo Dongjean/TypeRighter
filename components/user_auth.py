e1bcdf2e4a91e1cf7b04f512678a1f43e5a74d0e
Dongjean <shindongjin2004@gmail.com>
2026-05-26 14:53:28 +0800

    style: make user-auth page look nicer

--- components/user_auth.py
+++ components/user_auth.py
@@ -37,24 +37,34 @@ def build_user_auth(root, COLORS, FONTS):
     title_label.pack(fill="x", pady=0)
 
     # Login Hub Container
-    login_hub_container = tk.Frame(auth_frame, bg=COLORS["bg_input"], bd=1, highlightbackground=COLORS["border"], highlightthickness=1)
-    login_hub_container.pack()
+    login_hub_container = tk.Frame(auth_frame, bg=COLORS["bg_main"], bd=0)
+    login_hub_container.pack(expand=True)
 
-    username_editor = tk.Entry(login_hub_container, bg=COLORS["bg_input"], fg=COLORS["text_main"], insertbackground="white", bd=0, font=FONTS["font_subtitle"])
-    username_editor.pack(fill="both", expand=True)
-    # Bind the key release event inside text_editor to our reader function
+    # Username Frame
+    username_frame = tk.Frame(login_hub_container, bg=COLORS["bg_input"], bd=1, highlightbackground=COLORS["border"], highlightthickness=1)
+    username_frame.pack(fill="x", expand=True)
+    username_label = tk.Label(username_frame, text="Username: ", bg=COLORS["bg_input"], fg=COLORS["text_main"], bd=0, font=FONTS["font_subtitle"])
+    username_label.pack(side="left")
+    username_editor = tk.Entry(username_frame, bg=COLORS["bg_input"], fg=COLORS["text_main"], insertbackground="white", bd=0, font=FONTS["font_subtitle"])
+    username_editor.pack(side="right")
+    # Bind the key release event inside text editor to our reader function
     # tk.Text() has no native function to read text in real time, this is the best option
     username_editor.bind("<KeyRelease>", lambda event: on_key_release_username_editor(event, username_editor))
   
-    password_editor = tk.Entry(login_hub_container, bg=COLORS["bg_input"], fg=COLORS["text_main"], insertbackground="white", bd=0, font=FONTS["font_subtitle"])
-    password_editor.pack(fill="both", expand=True)
-    # Bind the key release event inside text_editor to our reader function
+    # Password Frame
+    password_frame = tk.Frame(login_hub_container, bg=COLORS["bg_input"], bd=1, highlightbackground=COLORS["border"], highlightthickness=1)
+    password_frame.pack(fill="x", expand=True)
+    password_label = tk.Label(password_frame, text="Password: ", bg=COLORS["bg_input"], fg=COLORS["text_main"], bd=0, font=FONTS["font_subtitle"])
+    password_label.pack(side="left")
+    password_editor = tk.Entry(password_frame, bg=COLORS["bg_input"], fg=COLORS["text_main"], insertbackground="white", bd=0, font=FONTS["font_subtitle"])
+    password_editor.pack(side="right")
+    # Bind the key release event inside text editor to our reader function
     # tk.Text() has no native function to read text in real time, this is the best option
     password_editor.bind("<KeyRelease>", lambda event: on_key_release_password_editor(event, password_editor))
 
     # Login Button
     login_button = tk.Button(login_hub_container, text="Login", bg=COLORS["border"], fg=COLORS["text_main"], bd=0, relief="flat", font=FONTS["font_subtitle"], command=(lambda: login(username, password)))
-    login_button.pack(side="right")
+    login_button.pack(side="bottom")
 
     # Clicking anywhere outside the text editor frame makes us lose active focus
     root.bind("<Button-1>", lambda event: event.widget.focus_set())
