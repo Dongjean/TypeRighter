0778d9165cf0fea89d774fe18cf007da1a1d55a5
Dongjean <shindongjin2004@gmail.com>
2026-05-26 19:52:55 +0800

    feat: add firebase connection utility file

---
+++ utils/firebase_app.py
@@ -0,0 +1,30 @@
+import pyrebase
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+FB_API_KEY = os.getenv("FB_API_KEY")
+FB_AUTH_DOMAIN = os.getenv("FB_AUTH_DOMAIN")
+FB_PROJECT_ID = os.getenv("FB_PROJECT_ID")
+FB_STORAGE_BUCKET = os.getenv("FB_STORAGE_BUCKET")
+FB_MESSAGING_SENDER_ID = os.getenv("FB_MESSAGING_SENDER_ID")
+FB_APP_ID = os.getenv("FB_APP_ID")
+FB_MEASUREMENT_ID = os.getenv("FB_MEASUREMENT_ID")
+
+firebaseConfig = {
+  "apiKey": FB_API_KEY,
+  "authDomain": FB_AUTH_DOMAIN,
+  "projectId": FB_PROJECT_ID,
+  "storageBucket": FB_STORAGE_BUCKET,
+  "messagingSenderId": FB_MESSAGING_SENDER_ID,
+  "appId": FB_APP_ID,
+  "measurementId": FB_MEASUREMENT_ID,
+
+  # Pyrebase demands a databaseUrl on initialisation
+  "databaseURL": "",
+}
+
+app = pyrebase.initialize_app(firebaseConfig)
+
+# Login Authenticator export
+auth = app.auth()
