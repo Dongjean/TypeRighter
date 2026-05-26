5f9de404a39e4494cbb8ee96fed4198c27ed499b
Dongjean <shindongjin2004@gmail.com>
2026-05-26 12:14:00 +0800

    feat: add window selector in navbar

--- components/navbar.py
+++ components/navbar.py
@@ -1,6 +1,14 @@
 import tkinter as tk
 from tkinter import font as tkfont
 
+curr_window = ""
+
+def change_window(selected_window):
+    global curr_window
+    if selected_window != curr_window:
+        print(selected_window)
+        curr_window = selected_window
+
 def build_navbar(root, COLORS, FONTS, WINDOWS, start_window):
 
     # Frame for navbar
@@ -9,9 +17,12 @@ def build_navbar(root, COLORS, FONTS, WINDOWS, start_window):
 
     # Display each window as a custom button
     navbar_frame.selected_window = tk.StringVar(value=start_window)
+    curr_window = start_window
     for key, value in WINDOWS.items():
-        btn = tk.Radiobutton(navbar_frame, text=value["name"], variable=navbar_frame.selected_window, value=key, bg=COLORS["border"], fg=COLORS["text_main"], bd=0, relief="flat", width=10, height=3, font=FONTS["font_subtitle"], selectcolor=COLORS["accent_blue"], activebackground=COLORS["accent_blue"], indicatoron=False)
+        btn = tk.Radiobutton(navbar_frame, text=value["name"], variable=navbar_frame.selected_window, value=key, bg=COLORS["border"], fg=COLORS["text_main"], bd=0, relief="flat", width=10, height=3, font=FONTS["font_subtitle"], selectcolor=COLORS["accent_blue"], activebackground=COLORS["accent_blue"], indicatoron=False, command=(lambda: change_window(navbar_frame.selected_window.get())))
         btn.pack(side="top")
+
+
 
     # Header Titles
     # Set to be at the top, centred (expand=False by default)
