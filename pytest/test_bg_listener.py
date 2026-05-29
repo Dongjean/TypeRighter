1a04a94ce277d3d1b7fa79f83c22b5db2d0e7b5c
Dongjean <shindongjin2004@gmail.com>
2026-05-29 14:58:56 +0800

    test: add cp_view closing to pytest

--- pytest/test_bg_listener.py
+++ pytest/test_bg_listener.py
@@ -677,3 +677,175 @@ def test_control_panel_key(subtests):
     for key, assertion in all_assertions.items():
         with subtests.test(msg=f"Asserting {key}"):
             assert assertion
+
+# As of here, the control panel view is open
+def test_close_control_panel():
+
+    # Check that when we close the control panel view, the overlay view is properly re-initialised and works fine
+
+    # Simulate Alt + f4 to close the control panel view
+    # Simulate Alt
+    key_simulator.press(keyboard.Key.alt_l)
+
+    # Simulate D while holding Ctrl down
+    key_simulator.press(keyboard.Key.f4)
+
+    # Release both
+    key_simulator.release(keyboard.Key.alt_l)
+    key_simulator.release(keyboard.Key.f4)
+
+    # Wait 150ms to allow the gui_queue polling loop to catch it
+    time.sleep(0.15) # 0.15s = 150ms
+    root.update()
+
+    # If the overlay works properly now, then control panel has been closed properly
+    # No need for asserts
+
+# Redo all of the overlay tests
+def test_redo_overlay_init(subtests):
+    
+    # Check for every property of root in the overlay that we set
+    
+    init_settings_test = get_overlay_init_tests()
+
+    # We initialise the overlay window as withdrawn first
+    is_withdrawn = root.state() == "withdrawn"
+
+    all_assertions = {
+        **init_settings_test,
+        "is_withdrawn": is_withdrawn
+    }
+    
+    for key, assertion in all_assertions.items():
+        with subtests.test(msg=f"Asserting {key}"):
+            assert assertion
+
+def test_redo_overlay_key(subtests):
+
+    # Test that Ctrl + D works
+    
+    # Simulate a keystroke event
+
+    # Simulate Ctrl
+    key_simulator.press(keyboard.Key.ctrl_l)
+
+    # Simulate D while holding Ctrl down
+    key_simulator.press("d")
+
+    # Release both
+    key_simulator.release(keyboard.Key.ctrl_l)
+    key_simulator.release("d")
+
+    # Manually update the root window
+    time.sleep(0.15)
+    root.update()
+
+    # The overlay should be on right now
+    # Check all of the overlay init settings, and then check that the window is not withdrawn
+    init_settings_test = get_overlay_init_tests()
+    
+    # root.deiconify() makes the state of root be "normal"
+    is_deiconify = root.state() == "normal"
+
+    all_assertions = {
+        **init_settings_test,
+        "is_deiconify": is_deiconify
+    }
+
+    for key, assertion in all_assertions.items():
+        with subtests.test(msg=f"Asserting {key}"):
+            assert assertion
+
+# Tests are run by pytest in the order they are defined
+# Thus, this will be run right after test_overlay_key()
+# Thus, Ctrl + D has already been pressed and overlay is on
+def test_redo_exit_key(subtests):
+
+    # Test that Ctrl + D, then "a" properly turns off the overlay
+
+    # Check that the overlay is properly initialised
+    init_settings_test = get_overlay_init_tests()
+
+    # Check that the overlay was on before
+    is_deiconify_before = root.state() == "normal"
+
+    # Simulate A
+    key_simulator.press("a")
+    # Release A
+    key_simulator.release("a")
+
+    # The gui_queue logic polls the queue once every 100ms
+    # Thus wait for 150ms minimally to allow the gui_queue to catch the keypress
+    time.sleep(0.15) # 0.15s = 150ms
+    root.update()
+
+    # Check that the overlay is withdrawn now
+    is_withdrawn = root.state() == "withdrawn"
+    print(root.state())
+    all_assertions = {
+        **init_settings_test,
+        "is_deiconify_before": is_deiconify_before,
+        "is_withdrawn": is_withdrawn
+    }
+
+    for key, assertion in all_assertions.items():
+        with subtests.test(msg=f"Asserting {key}"):
+            assert assertion
+
+def test_redo_wrong_key(subtests):
+
+    # Check that the screen properly goes red, then turns off when a wrong key is pressed after Ctrl + D
+
+    # Check that the overlay is properly initialised
+    init_settings_test = get_overlay_init_tests()
+    
+    # Press Ctrl + D now
+    # Simulate Ctrl
+    key_simulator.press(keyboard.Key.ctrl_l)
+
+    # Simulate D while holding Ctrl down
+    key_simulator.press("d")
+
+    # Release both
+    key_simulator.release(keyboard.Key.ctrl_l)
+    key_simulator.release("d")
+    
+    time.sleep(0.15)
+    root.update()
+
+    # Check that the overlay was on before
+    is_deiconify_before = root.state() == "normal"
+    # print(root.state())
+
+    # Let the wrong keypress be F
+    # Simulate F
+    key_simulator.press("f")
+    # Release F
+    key_simulator.release("f")
+
+    # Wait 150ms to allow the gui_queue polling loop to catch it
+    time.sleep(0.15) # 0.15s = 150ms
+    root.update()
+
+    # Overlay should be red now
+    canvas = root.nametowidget(".overlay")
+    is_red = canvas.itemcget("overlay", "outline") == "red"
+
+    # Red overlay is flashed for 1s
+    # Thus wait 1.05s to allow the red overlay to go away
+    time.sleep(1.05)
+    root.update()
+
+    # Overlay should be withdrawn now
+    is_withdrawn = root.state() == "withdrawn"
+
+    all_assertions = {
+        **init_settings_test,
+        "is_deiconify_before": is_deiconify_before,
+        "is_red": is_red,
+        "is_withdrawn": is_withdrawn
+    }
+
+    for key, assertion in all_assertions.items():
+        with subtests.test(msg=f"Asserting {key}"):
+            assert assertion
