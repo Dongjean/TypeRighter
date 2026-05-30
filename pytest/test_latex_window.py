c565a47a46a40547d7472742ceab011d9bfdb519
Dongjean <shindongjin2004@gmail.com>
2026-05-30 17:13:15 +0800

    test: add image copy check for latex output

--- pytest/test_latex_window.py
+++ pytest/test_latex_window.py
@@ -1,6 +1,9 @@
 import pytest
 from tkinter import font as tkfont
 import tkinter as tk
+import win32clipboard as clip
+import win32con
+from io import BytesIO
 
 import sys
 
@@ -603,7 +606,7 @@ def test_latex_output_enter(test_env, subtests):
     text_editor.focus_set()
 
     # Simulate typing out a latex formula
-    latex_code_sample = r"\frac{1}{2}"
+    latex_code_sample = r"\frac{1}{3}"
     for char in latex_code_sample:
         key_simulator.press(char)
     
@@ -627,6 +630,25 @@ def test_latex_output_enter(test_env, subtests):
     # LaTeX output is properly displayed iff both of these have a value, if not something is wrong
     is_latex_output_displayed = (tk_img != None) and (pil_img != None)
 
+    # Check that an image is properly copied to clipboard
+    clip_data = None
+    clip.OpenClipboard()
+    try:
+        clip_data = clip.GetClipboardData(win32con.CF_DIB)
+    finally:
+        clip.CloseClipboard()
+    # Check if an image is copied at all
+    is_img_copied = (clip_data != None)
+    
+    # Check if the image copied is the right image
+    # Get the dib data from pil_img
+    rgba_img = pil_img.convert("RGBA")
+    output = BytesIO()
+    rgba_img.save(output, "BMP")
+    dib_data = output.getvalue()[14:]
+    output.close()
+    is_latex_img_copied = (pil_img != None) and (clip_data == dib_data)
+
     # Simulate Alt + f4 to close the control panel view
     # Simulate Alt
     key_simulator.press(keyboard.Key.alt_l)
@@ -643,7 +665,9 @@ def test_latex_output_enter(test_env, subtests):
     root.update()
 
     all_assertions = {
-        "is_latex_output_displayed": is_latex_output_displayed
+        "is_latex_output_displayed": is_latex_output_displayed,
+        "is_img_copied": is_img_copied,
+        "is_latex_img_copied": is_latex_img_copied
     }
 
     for key, assertion in all_assertions.items():
