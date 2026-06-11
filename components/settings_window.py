4db7ba39366b406515ddae2965ec60021af61f73
Dongjean <shindongjin2004@gmail.com>
2026-06-11 16:28:36 +0800

    feat: add proper dynamic subwindow label to settings subwindows

--- components/settings_window.py
+++ components/settings_window.py
@@ -25,7 +25,7 @@ init_functions = {
     "another": build_another_setting,
 }
 
-def select_setting(selected_setting, settings_container, COLORS, FONTS):
+def select_setting(selected_setting, settings_container, subwindow_label, COLORS, FONTS):
     global curr_subwindow
     settings_subwindow_container = settings_container.nametowidget("settings_subwindow_container")
     settings_selection_container = settings_container.nametowidget("settings_selection_container")
@@ -41,7 +41,13 @@ def select_setting(selected_setting, settings_container, COLORS, FONTS):
     settings_subwindow_container.pack(fill="both", expand=True)
     settings_selection_container.pack_forget()
 
-def back(settings_frame):
+    # Update the subwindow label
+    new_label = ""
+    if curr_subwindow in SETTINGS:
+        new_label = SETTINGS[curr_subwindow]["name"]
+    subwindow_label.config(text=new_label)
+
+def back(settings_frame, subwindow_label):
     global curr_subwindow
     settings_container = settings_frame.nametowidget("settings_container")
     settings_subwindow_container = settings_container.nametowidget("settings_subwindow_container")
@@ -54,6 +60,9 @@ def back(settings_frame):
 
     settings_subwindow_container.pack_forget()
     settings_selection_container.pack(fill="both", expand=True)
+
+    # Update the subwindow label
+    subwindow_label.config(text="All Settings")
 
 def build_settings_window(root, COLORS, FONTS):
 
@@ -71,11 +80,11 @@ def build_settings_window(root, COLORS, FONTS):
     subwindow_header.pack(fill="x", pady=0)
 
     # Subwindow Label
-    subwindow_label = tk.Label(subwindow_header, text="route", fg=COLORS["text_muted"], bg=COLORS["bg_main"], font=FONTS["font_subtitle"], name="subwindow_label")
+    subwindow_label = tk.Label(subwindow_header, text="All Settings", fg=COLORS["text_muted"], bg=COLORS["bg_main"], font=FONTS["font_subtitle"], name="subwindow_label")
     subwindow_label.pack(fill="none", anchor="center", side="right", expand=True, pady=0)
 
     # Back Button
-    back_button = tk.Button(subwindow_header, text="Back", bg=COLORS["border"], fg=COLORS["text_main"], bd=0, relief="flat", font=FONTS["font_subtitle"], activebackground=COLORS["accent_blue"], command=(lambda: back(settings_frame)), name="back_button")
+    back_button = tk.Button(subwindow_header, text="Back", bg=COLORS["border"], fg=COLORS["text_main"], bd=0, relief="flat", font=FONTS["font_subtitle"], activebackground=COLORS["accent_blue"], command=(lambda: back(settings_frame, subwindow_label)), name="back_button")
     back_button.pack(fill="none", anchor="w", side="left", pady=0)
 
     # Settings Container to contain the settings selector and settings sub-windows
@@ -94,7 +103,7 @@ def build_settings_window(root, COLORS, FONTS):
 
     # Display each window as a custom button
     for key, value in SETTINGS.items():
-        btn = tk.Button(settings_selection_container, text=value["name"], bg=COLORS["border"], fg=COLORS["text_main"], bd=0, relief="flat", height=3, font=FONTS["font_subtitle"], activebackground=COLORS["accent_blue"], command=(lambda subwindow=key: select_setting(subwindow, settings_container, COLORS, FONTS)), name=key)
+        btn = tk.Button(settings_selection_container, text=value["name"], bg=COLORS["border"], fg=COLORS["text_main"], bd=0, relief="flat", height=3, font=FONTS["font_subtitle"], activebackground=COLORS["accent_blue"], command=(lambda subwindow=key: select_setting(subwindow, settings_container, subwindow_label, COLORS, FONTS)), name=key)
         btn.pack(side="top", fill="x")
     
     if is_selecting:
