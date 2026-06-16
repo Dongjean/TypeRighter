f6e4c5eb1fb4bc96661e9188b438c7152f0565bf
Dongjean <shindongjin2004@gmail.com>
2026-06-16 18:56:20 +0800

    feat: add expand/retract sub-setting in preferences setting

--- components/preferences_setting.py
+++ components/preferences_setting.py
@@ -24,16 +24,26 @@ def build_preferences_setting(settings_subwindow_container, COLORS, FONTS):
     template_selector.set(curr_templates[0])
     template_selector.pack()
 
-    # Display the Current Keybinds Under the Selected Template
+    # Display the Keybinds Under the Selected Template
     
     # Current Keybinds Container
-    curr_keybinds_container = tk.Frame(preferences_frame, bg=COLORS["bg_input"], name="curr_keybinds_container")
-    curr_keybinds_container.pack()
+    keybinds_container = tk.Frame(preferences_frame, bg=COLORS["bg_input"], name="curr_keybinds_container")
+    keybinds_container.pack()
     
     # Keybinds Label
-    curr_keybinds_label = tk.Label(curr_keybinds_container, bg=COLORS["bg_input"], font=FONTS["font_subtitle"], fg=COLORS["text_main"], text="Current Keybinds", name="curr_keybinds_label")
-    curr_keybinds_label.pack()
+    keybinds_label = tk.Label(keybinds_container, bg=COLORS["bg_input"], font=FONTS["font_subtitle"], fg=COLORS["text_main"], text="Current Keybinds", name="keybinds_label")
+    keybinds_label.pack()
 
+    def toggle_widget_visibility(widget):
+        
+        # If the widget is already visible, turn it off
+        if widget.winfo_ismapped():
+            widget.pack_forget()
+        else:
+            widget.pack()
+            
+    keybinds_display_container = tk.Frame(keybinds_container, bg=COLORS["bg_input"], name="phrasebinds_display_container")
+    
     # Get the Dictionary of Keybinds
     curr_keybinds = shortcuts_unicode.all_bindings()
 
@@ -41,7 +51,7 @@ def build_preferences_setting(settings_subwindow_container, COLORS, FONTS):
     for key, bind in curr_keybinds.items():
         
         # This Keybind's Container
-        keybind_container = tk.Frame(curr_keybinds_container, bg=COLORS["bg_input"], name=f"{key}_keybind_container")
+        keybind_container = tk.Frame(keybinds_display_container, bg=COLORS["bg_input"], name=f"{key}_keybind_container")
         keybind_container.pack(pady=2, anchor="w")
 
         # The Key Label
@@ -56,24 +66,28 @@ def build_preferences_setting(settings_subwindow_container, COLORS, FONTS):
         keybind_unbinder =tk.Button(keybind_container, text="Unbind", fg=COLORS["action_green"], bg=COLORS["bg_input"], font=FONTS["font_subtitle"], bd=0, command=None, name=f"{key}_keybind_unbinder")
         keybind_unbinder.pack(side="right", padx=8)
     
+    keybinds_label.bind("<Button-1>", lambda e: toggle_widget_visibility(keybinds_display_container))
+
     # Current Phrase Bindings Container
-    curr_phrasebinds_container = tk.Frame(preferences_frame, bg=COLORS["bg_input"], name="curr_phrasebinds_container")
-    curr_phrasebinds_container.pack()
+    phrasebinds_container = tk.Frame(preferences_frame, bg=COLORS["bg_input"], name="phrasebinds_container")
+    phrasebinds_container.pack()
 
     # Phrase Bindings Label
-    curr_phrasebinds_label = tk.Label(curr_phrasebinds_container, bg=COLORS["bg_input"], font=FONTS["font_subtitle"], fg=COLORS["text_main"], text="Current Phrase Bindings", name="curr_phrasebinds_label")
-    curr_phrasebinds_label.pack()
+    phrasebinds_label = tk.Label(phrasebinds_container, bg=COLORS["bg_input"], font=FONTS["font_subtitle"], fg=COLORS["text_main"], text="Current Phrase Bindings", name="phrasebinds_label")
+    phrasebinds_label.pack()
 
     # Fake list of Phrase Bindings
     curr_phrasebinds = {
         "alpha": "α",
     }
 
+    phrasebinds_display_container = tk.Frame(phrasebinds_container, bg=COLORS["bg_input"], name="phrasebinds_display_container")
+
     # Show Each Phrase Binding
     for phrase, bind in curr_phrasebinds.items():
         
         # This Phrase Binding's Container
-        phrasebind_container = tk.Frame(curr_phrasebinds_container, bg=COLORS["bg_input"], name=f"{phrase}_phrasebind_container")
+        phrasebind_container = tk.Frame(phrasebinds_display_container, bg=COLORS["bg_input"], name=f"{phrase}_phrasebind_container")
         phrasebind_container.pack()
 
         # The Phrase Label
@@ -87,6 +101,8 @@ def build_preferences_setting(settings_subwindow_container, COLORS, FONTS):
         # This Phrase Binding's Unbinder
         phrasebind_unbinder =tk.Button(phrasebind_container, text="Unbind", fg=COLORS["action_green"], bg=COLORS["bg_input"], font=FONTS["font_subtitle"], bd=0, command=None, name=f"{phrase}_phrasebind_unbinder")
         phrasebind_unbinder.pack(side="right", padx=8)
+    
+    phrasebinds_label.bind("<Button-1>", lambda e: toggle_widget_visibility(phrasebinds_display_container))
 
 def destroy_preferences_setting(settings_subwindow_container):
     for widget in settings_subwindow_container.winfo_children():
