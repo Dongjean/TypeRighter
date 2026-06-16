68434cf8131069bb1684f6a87944b3e68823bad3
Dongjean <shindongjin2004@gmail.com>
2026-06-16 19:49:36 +0800

    feat: add real-time keybind rebinding and unbinding

--- components/preferences_setting.py
+++ components/preferences_setting.py
@@ -3,6 +3,89 @@ from tkinter import ttk
 
 import utils.shortcuts_unicode as shortcuts_unicode
 
+def toggle_widget_visibility(widget):
+    
+    # If the widget is already visible, turn it off
+    if widget.winfo_ismapped():
+        widget.pack_forget()
+    else:
+        widget.pack()
+
+def _refresh_keybinds(keybinds_display_container, preferences_frame, COLORS, FONTS):
+    
+    # Get the new keybinds
+    curr_keybinds = shortcuts_unicode.all_bindings()
+    print(curr_keybinds)
+    # First Delete Existing Keybinds
+    for widget in keybinds_display_container.winfo_children():
+        widget.destroy()
+
+    # Then Create the new Keybinds
+    for key, bind in curr_keybinds.items():
+        
+        # This Keybind's Container
+        keybind_container = tk.Frame(keybinds_display_container, bg=COLORS["bg_input"], name=f"{key}_keybind_container")
+        keybind_container.pack(pady=2, anchor="w")
+
+        # The Key Label
+        key_label = tk.Label(keybind_container, bg=COLORS["bg_input"], font=FONTS["font_subtitle"], highlightbackground="white", highlightthickness=1, fg=COLORS["text_main"], text=key, name=f"{key}_key_label")
+        key_label.pack(side="left")
+
+        # The Corresponding Bind Label
+        bind_label = tk.Label(keybind_container, bg=COLORS["bg_input"], font=FONTS["font_subtitle"], fg=COLORS["text_main"], text=bind, name=f"{key}_bind_label")
+        bind_label.pack(side="left")
+
+        # This Keybind's Unbinder
+        # Display this iff it isnt one of the protected keys
+        if bind not in ["Exit App", "Close Overlay", "Control Panel", "Breakout Key"]:
+            keybind_unbinder = tk.Button(keybind_container, text="Unbind", fg=COLORS["action_green"], bg=COLORS["bg_input"], font=FONTS["font_subtitle"], bd=0, command=lambda COLORS=COLORS, FONTS=FONTS, key=key: _unbind_key(keybinds_display_container, preferences_frame, COLORS, FONTS, key), name=f"{key}_keybind_unbinder")
+            keybind_unbinder.pack(side="right", padx=8)
+
+        # This Keybind's Rebinder
+        keybind_rebinder = tk.Button(keybind_container, text="Rebind", fg=COLORS["action_green"], bg=COLORS["bg_input"], font=FONTS["font_subtitle"], bd=0, command = lambda to_bind=bind, old_key=key: _rebind_key(preferences_frame, keybinds_display_container, to_bind, old_key, COLORS, FONTS), name=f"{key}_keybind_rebinder")
+        keybind_rebinder.pack(side="right")
+
+# Keybind popup
+def _rebind_key(preferences_frame, keybinds_display_container, to_bind, old_key, COLORS, FONTS): 
+    popup = tk.Toplevel(preferences_frame, bg = COLORS["bg_main"]) 
+    popup.title("Re-bind symbol to key")
+    popup.configure(padx=20, pady=20)
+    popup.transient(preferences_frame)
+    popup.grab_set()
+    popup.resizable(False, False)
+
+    popuptitle = tk.Label(popup, text=f"Re-bind a key to {to_bind}", bg = COLORS["bg_main"], fg = COLORS["text_main"], font=FONTS["font_title"])
+    popuptitle.pack(pady=(0, 10))
+
+    prompt = tk.Label(popup, text="Press any key ...", bg = COLORS["bg_main"], fg = COLORS["text_main"], font=FONTS["font_subtitle"])
+    prompt.pack(pady=15)
+
+    # Capture keystroke to bind
+    def on_key_press(event):
+        if not event.char or not event.char.strip(): 
+            return
+        # Before we bind, unbind the existing keybind
+        ok = shortcuts_unicode.remove_binding(old_key)
+        if not ok:
+            popup.destroy()
+            return
+
+        # Call shortcut function
+        ok, message = shortcuts_unicode.set_binding(event.char, to_bind)
+        if ok: 
+            popup.destroy()
+            _refresh_keybinds(keybinds_display_container, preferences_frame, COLORS, FONTS)
+        else:
+            prompt.config(text=message, font=FONTS["font_subtitle"], fg="#FF0000")
+
+    popup.bind("<Key>", on_key_press)
+    popup.bind("<Escape>", lambda e: popup.destroy())
+    popup.focus_force()
+
+def _unbind_key(keybinds_display_container, preferences_frame, COLORS, FONTS, key):
+    shortcuts_unicode.remove_binding(key)
+    _refresh_keybinds(keybinds_display_container, preferences_frame, COLORS, FONTS)
+
 def build_preferences_setting(settings_subwindow_container, COLORS, FONTS):
     
     # Preferences Frame
@@ -33,14 +116,6 @@ def build_preferences_setting(settings_subwindow_container, COLORS, FONTS):
     # Keybinds Label
     keybinds_label = tk.Label(keybinds_container, bg=COLORS["bg_input"], font=FONTS["font_subtitle"], fg=COLORS["text_main"], text="Current Keybinds", name="keybinds_label")
     keybinds_label.pack()
-
-    def toggle_widget_visibility(widget):
-        
-        # If the widget is already visible, turn it off
-        if widget.winfo_ismapped():
-            widget.pack_forget()
-        else:
-            widget.pack()
             
     keybinds_display_container = tk.Frame(keybinds_container, bg=COLORS["bg_input"], name="phrasebinds_display_container")
     
@@ -65,8 +140,12 @@ def build_preferences_setting(settings_subwindow_container, COLORS, FONTS):
         # This Keybind's Unbinder
         # Display this iff it isnt one of the protected keys
         if bind not in ["Exit App", "Close Overlay", "Control Panel", "Breakout Key"]:
-            keybind_unbinder = tk.Button(keybind_container, text="Unbind", fg=COLORS["action_green"], bg=COLORS["bg_input"], font=FONTS["font_subtitle"], bd=0, command=lambda key=key: shortcuts_unicode.remove_binding(key), name=f"{key}_keybind_unbinder")
+            keybind_unbinder = tk.Button(keybind_container, text="Unbind", fg=COLORS["action_green"], bg=COLORS["bg_input"], font=FONTS["font_subtitle"], bd=0, command=lambda COLORS=COLORS, FONTS=FONTS, key=key: _unbind_key(keybinds_display_container, preferences_frame, COLORS, FONTS, key), name=f"{key}_keybind_unbinder")
             keybind_unbinder.pack(side="right", padx=8)
+
+        # This Keybind's Rebinder
+        keybind_rebinder = tk.Button(keybind_container, text="Rebind", fg=COLORS["action_green"], bg=COLORS["bg_input"], font=FONTS["font_subtitle"], bd=0, command = lambda to_bind=bind, old_key=key: _rebind_key(preferences_frame, keybinds_display_container, to_bind, old_key, COLORS, FONTS), name=f"{key}_keybind_rebinder")
+        keybind_rebinder.pack(side="right")
     
     keybinds_label.bind("<Button-1>", lambda e: toggle_widget_visibility(keybinds_display_container))
 
