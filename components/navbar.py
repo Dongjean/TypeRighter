85e187059c37257b661000a65e96406ac23f4ef3
Dongjean <shindongjin2004@gmail.com>
2026-07-19 12:40:56 +0800

    feat: add templates switching in navbar

--- components/navbar.py
+++ components/navbar.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 
 from tkinter import font as tkfont
 
@@ -10,6 +11,12 @@ import components.user_auth as user_auth
 import components.unicode_searchpanel as unicode_search
 import components.settings_window as settings_window
 
+import main as main
+import utils.settings as settings
+import utils.templates as templates
+import utils.shortcuts_unicode as shortcuts_unicode
+import utils.auth as auth
+
 curr_window = ""
 init_functions = {
     "latex-workspace": latex_workspace.build_latex_workspace,
@@ -18,15 +25,6 @@ init_functions = {
     "settings-window": settings_window.build_settings_window,
 }
 
-def change_window(selected_window, root, COLORS, FONTS):
-    global curr_window
-    if selected_window != curr_window:
-        print(selected_window)
-        curr_window = selected_window
-        if selected_window in init_functions:
-            init_functions[selected_window](root=root, COLORS=COLORS, FONTS=FONTS)
-        else:
-            print(f"No function found to initialise {selected_window}")
 destroy_functions = {
     "latex-workspace": latex_workspace.destroy_latex_workspace,
     "user-auth": user_auth.destroy_user_auth,
@@ -54,6 +52,37 @@ def change_window(selected_window, root, COLORS, FONTS):
         else:
             print(f"No function found to initialise {curr_window}")
 
+def reload_window(selected_window, root, COLORS, FONTS):
+    global curr_window
+
+    # Do things iff the window is unchanged (for reloading windows)
+    if selected_window == curr_window:
+        
+        # First, destroy the window
+        if selected_window in destroy_functions:
+            destroy_functions[selected_window](root=root)
+        else:
+            print(f"No function found to destroy {selected_window}")
+        
+        # Then, initialise the new window
+        if selected_window in init_functions:
+            init_functions[selected_window](root=root, COLORS=COLORS, FONTS=FONTS)
+        else:
+            print(f"No function found to initialise {selected_window}")
+
+def on_template_selection(event, template_selector, root, COLORS, FONTS):
+    
+    # Get the current value from the combobox
+    selected_template = template_selector.get()
+    
+    if selected_template:
+        templates.use_template(selected_template)
+        curr_user, e = auth.get_email()
+        settings.set_setting("curr_template", selected_template, curr_user)
+        shortcuts_unicode.load()
+        main.update_breakout_key()
+        reload_window(curr_window, root, COLORS, FONTS)
+
 def build_navbar(root, COLORS, FONTS, WINDOWS, start_window):
 
     # Frame for navbar
@@ -65,6 +94,19 @@ def build_navbar(root, COLORS, FONTS, WINDOWS, start_window):
     for key, value in WINDOWS.items():
         btn = tk.Radiobutton(navbar_frame, text=value["name"], variable=navbar_frame.selected_window, value=key, bg=COLORS["border"], fg=COLORS["text_main"], bd=0, relief="flat", width=10, height=3, font=FONTS["font_subtitle"], selectcolor=COLORS["accent_blue"], activebackground=COLORS["accent_blue"], indicatoron=False, command=(lambda: change_window(navbar_frame.selected_window.get(), root, COLORS, FONTS)), name=key)
         btn.pack(side="top")
+    
+    email, e = auth.get_email()
+    if email:
+        # List of templates
+        curr_templates = templates.all_templates(email)
+        curr_template_names = list(curr_templates)
+        
+        # Template Selector Dropdown Menu
+        template_selector = ttk.Combobox(navbar_frame, values=curr_template_names, state="readonly")
+        curr_selected_template = settings.lookup_setting("curr_template")
+        template_selector.set(curr_selected_template)
+        template_selector.pack()
+        template_selector.bind("<<ComboboxSelected>>", lambda event: on_template_selection(event, template_selector, root, COLORS, FONTS))
 
     # Manually initialise the first window
     # Clear curr_window first jic we exited control panel view and re-entere
