345aae7b140d01bcfe27cc20d2589582bef8d8dd
Dongjean <shindongjin2004@gmail.com>
2026-07-21 09:15:28 +0800

    fix: properly add test functions

--- pytest/tester_functions/bg_listener_testers.py
+++ pytest/tester_functions/bg_listener_testers.py
@@ -16,7 +16,7 @@ def helper_test_bg_listener_on(test_env, subtests):
     key_simulator.press(keyboard.Key.alt_l)
     key_simulator.press(keyboard.Key.space)
 
-    wait(root, 0.05)
+    wait(root, 0.1)
 
     key_simulator.release(keyboard.Key.ctrl_l)
     key_simulator.release(keyboard.Key.alt_l)
@@ -31,6 +31,36 @@ def helper_test_bg_listener_on(test_env, subtests):
     all_assertions = {
         **init_settings_test,
         "is_deiconify": is_deiconify
+    }
+    
+    for key, assertion in all_assertions.items():
+        with subtests.test(msg=f"Asserting {key}"):
+            assert assertion
+
+def helper_test_bg_listener_off(test_env, subtests):
+    
+    root, sw, sh, FONTS, COLORS, WINDOWS = test_env
+
+    # Trigger the COMBINATION keybind to turn off the overlay
+    key_simulator.press(keyboard.Key.ctrl_l)
+    key_simulator.press(keyboard.Key.alt_l)
+    key_simulator.press(keyboard.Key.space)
+
+    wait(root, 0.1)
+
+    key_simulator.release(keyboard.Key.ctrl_l)
+    key_simulator.release(keyboard.Key.alt_l)
+    key_simulator.release(keyboard.Key.space)
+
+    # Check that the overlay is properly initiated
+    init_settings_test = get_overlay_init_tests(root, sw, sh)
+
+    # We initialise the overlay window as withdrawn first
+    is_withdrawn = root.state() == "withdrawn"
+
+    all_assertions = {
+        **init_settings_test,
+        "is_withdrawn": is_withdrawn
     }
     
     for key, assertion in all_assertions.items():
--- pytest/tester_functions/main_testers.py
+++ pytest/tester_functions/main_testers.py
@@ -1,4 +1,4 @@
-from tester_functions.bg_listener_testers import helper_test_bg_listener_on
+from tester_functions.bg_listener_testers import helper_test_bg_listener_on, helper_test_bg_listener_off
 from tester_functions.overlay_view_testers import helper_test_overlay_init, helper_test_overlay_key, helper_test_exit_key, helper_test_wrong_key
 from tester_functions.cp_view_testers import helper_test_control_panel_key, helper_test_close_control_panel
 from tester_functions.latex_window_testers import helper_test_latex_output_enter
@@ -6,7 +6,10 @@ from tester_functions.navbar_component_testers import helper_test_navbar
 from tester_functions.unicode_window_testers import helper_test_unicode_search_menu, helper_test_unicode_search_function,helper_test_unicode_copy_paste, helper_test_unicode_copy_via_overlay
 
 def bg_listener_tester(test_env, subtests):
-    helper_test_bg_listener_on
+    
+    helper_test_bg_listener_on(test_env, subtests)
+    
+    helper_test_bg_listener_off(test_env, subtests)
 
 # def overlay_tester(test_env, subtests):
 
