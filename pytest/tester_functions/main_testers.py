227c6c16a5cdafe02e487d4956d3b8e3e6757c96
Dongjean <shindongjin2004@gmail.com>
2026-07-25 18:14:10 +0800

    fix: no need to test copy paste via overlay as it is done in overlay tests already

--- pytest/tester_functions/main_testers.py
+++ pytest/tester_functions/main_testers.py
@@ -3,7 +3,7 @@ from tester_functions.overlay_view_testers import helper_test_overlay_breakout_k
 from tester_functions.cp_view_testers import helper_test_control_panel_key, helper_test_close_control_panel
 from tester_functions.latex_window_testers import helper_test_latex_output_enter
 from tester_functions.navbar_component_testers import helper_test_navbar
-from tester_functions.unicode_window_testers import helper_test_unicode_search_menu, helper_test_unicode_search_function,helper_test_unicode_copy_paste, helper_test_unicode_copy_via_overlay
+from tester_functions.unicode_window_testers import helper_test_unicode_search_menu, helper_test_unicode_search_function,helper_test_unicode_copy_paste
 
 def bg_listener_tester(test_env, subtests):
     
@@ -51,6 +51,6 @@ def unicode_tester(test_env, subtests):
 
     helper_test_unicode_copy_paste(subtests)
 
-    helper_test_unicode_copy_via_overlay(test_env, subtests)
-    
+    helper_test_close_control_panel(test_env)
+
     helper_test_bg_listener_off(test_env, subtests)
