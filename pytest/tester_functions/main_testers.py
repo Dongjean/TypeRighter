8cc4ec03ec6868f58496d3af5fa70ab857da1888
Dongjean <shindongjin2004@gmail.com>
2026-07-25 17:59:46 +0800

    test: allow to run any test function in any order for more robust debugging

--- pytest/tester_functions/main_testers.py
+++ pytest/tester_functions/main_testers.py
@@ -26,17 +26,25 @@ def latex_tester(test_env, subtests):
     helper_test_control_panel_key(test_env, subtests)
 
     helper_test_latex_output_enter(test_env, subtests)
+    
+    helper_test_bg_listener_off(test_env, subtests)
 
 def navbar_tester(test_env, subtests):
     
+    helper_test_bg_listener_on(test_env, subtests)
+
     helper_test_control_panel_key(test_env, subtests)
 
     helper_test_navbar(test_env, subtests)
+    
+    helper_test_bg_listener_off(test_env, subtests)
 
 def unicode_tester(test_env, subtests):
 
+    helper_test_bg_listener_on(test_env, subtests)
+
     helper_test_control_panel_key(test_env, subtests)
-    
+
     helper_test_unicode_search_menu(test_env, subtests)
 
     helper_test_unicode_search_function(subtests)
@@ -44,3 +52,5 @@ def unicode_tester(test_env, subtests):
     helper_test_unicode_copy_paste(subtests)
 
     helper_test_unicode_copy_via_overlay(test_env, subtests)
+    
+    helper_test_bg_listener_off(test_env, subtests)
